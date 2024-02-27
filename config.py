fb0dc2b5da60b89f49b52aec6f9cc59b17158f0f
antonysavache <89783847+antonysavache@users.noreply.github.com>
2024-02-27 14:56:04 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+DB_URL = 'postgresql://postgres:CDb3FAgcdGAc5Ac1EEe2C3G3F42gGGA1@monorail.proxy.rlwy.net:43447/railway'
+PGDATABASE = 'railway'
+PGHOST = 'monorail.proxy.rlwy.net'
+PGPASS = 'CDb3FAgcdGAc5Ac1EEe2C3G3F42gGGA1'
+PGPORT = 43447
+PGUSER = 'postgres'
